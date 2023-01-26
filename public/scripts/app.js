3d452d6a7048b26f0f3b57e22506b0aa85978498
bruno ugwu <brunougwu5658@gmail.com>
2023-01-26 21:46:37 +0100

    updated decision

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -19,7 +19,7 @@ var IndecisionApp = function (_React$Component) {
     _this.handleDeleteOptions = _this.handleDeleteOptions.bind(_this);
     _this.handlePick = _this.handlePick.bind(_this);
     _this.handleAddOption = _this.handleAddOption.bind(_this);
-    _this.handleDeleteOption = _this.handleDeleteOption(_this);
+    _this.handleDeleteOption = _this.handleDeleteOption.bind(_this);
     _this.state = {
       options: props.options
     };
@@ -30,13 +30,15 @@ var IndecisionApp = function (_React$Component) {
     key: "handleDeleteOptions",
     value: function handleDeleteOptions() {
       this.setState(function () {
-        return { option: [] };
+        return { options: [] };
       });
     }
   }, {
     key: "handleDeleteOption",
-    value: function handleDeleteOption() {
-      console.log("delete option");
+    value: function handleDeleteOption(option) {
+      this.setState(function (prevState) {
+        return {};
+      });
     }
   }, {
     key: "handlePick",
@@ -135,22 +137,20 @@ var Options = function Options(props) {
       return React.createElement(Option, {
         key: option,
         optionText: option,
-        handleDeleteOption: undefined.handleDeleteOption
+        handleDeleteOption: props.handleDeleteOption
       });
-    }),
-    React.createElement(Option, null)
+    })
   );
 };
 
 var Option = function Option(props) {
-  return;
-  React.createElement(
+  return React.createElement(
     "div",
     null,
     props.optionText,
     React.createElement(
       "button",
-      { onClick: props.handleDeleteOption },
+      { onClick: function onClick(e) {} },
       "Remove"
     )
   );
